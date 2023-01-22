2d34cc9e67c919c9f86d1ed5da29b2b445fd039b
xmatsunichi <xmatsunichi@gmail.com>
2023-01-22 08:45:33 +0800

    first commit

---
+++ plugins/donasi1.js
@@ -0,0 +1,35 @@
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+//let handler = async(m, { conn, text, usedPrefix, command }) => {
+//let pp = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image')
+
+let str = `
+
+┌─「 Donasi • Pulsa 」
+│ • *Telkomsel:* [${global.ppulsa}]
+❏────
+
+┌─「 Donasi • Non Pulsa 」
+│ • *Dana:* [${global.pdana}]
+│ • *Gopay:* [${global.pgopay}]
+│ • *Ovo:* [${global.povo}]
+│ • *Link Aja:* [${global.plinkaja}]
+❏────`
+let wibu = `https://telegra.ph/file/ef6105e92cab05d1a5eb3.jpg` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.send3ButtonDoc(m.chat, str, botdate,'Sewa Bot','.sewa','QR+PAYMENT','.donasi1','Menu','.menu', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "tautanwa",
+    mediaType: "tautanwa",
+    description: "https://www.instagram.com/anshorfalahi/", 
+    title: 'SUBSCRIBE YT',
+    body: bottime,
+    thumbnail: thumb,
+    sourceUrl: syt
+  }
+  } }) 
+          }
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+export default handler
